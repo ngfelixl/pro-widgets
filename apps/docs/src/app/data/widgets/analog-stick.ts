9ba0a274cbcd19cc64bebb0384697fdfc34fa144
Felix Lemke <flemke.dev@gmail.com>
2019-03-28 10:30:19 +0100

    docs: import mat-icons, use partly randomized/sin widget input

--- apps/docs/src/app/data/widgets/analog-stick.ts
+++ apps/docs/src/app/data/widgets/analog-stick.ts
@@ -1,16 +1,32 @@
 import { Widget } from '@docs/app/models/widget';
 import { AnalogStickComponent } from '@pro-widgets';
 
+let response = [0, 0];
+let lastTime = 0;
+
 export const analogStick: Widget = {
   name: 'Analog Stick',
   component: AnalogStickComponent,
   dataFieldName: 'value',
   valueGenerator: () => {
-    const angle = new Date().getTime() / 800;
-    return [
-      Math.round((Math.sin(angle / 3) * 30 + 50) * 100) / 100,
-      Math.round((Math.cos(angle) * 40 + 50) * 100) / 100
-    ];
+    const time = new Date().getTime() / 1000;
+    if (time%20 > 10) {
+      const angle = new Date().getTime() / 800;
+      return [
+        Math.round((Math.sin(angle / 3) * 30 + 50) * 100) / 100,
+        Math.round((Math.cos(angle) * 40 + 50) * 100) / 100
+      ];
+    } else {
+      const roundedTime = (Math.round(time));
+      if (roundedTime%1 === 0 && lastTime !== roundedTime) {
+        response = [
+          Math.round(Math.random() * 10000) / 100,
+          Math.round(Math.random() * 10000) / 100
+        ];
+        lastTime = roundedTime;
+      }
+      return response;
+    }
   },
   module: 'AnalogStickModule',
   tabs: [
